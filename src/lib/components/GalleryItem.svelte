7d220962b841fedb483315930368f4944b298042
Lifulifu <andycscsmaple@gmail.com>
2025-01-26 22:22:56 +0800

    fix

--- src/lib/components/GalleryItem.svelte
+++ src/lib/components/GalleryItem.svelte
@@ -8,7 +8,6 @@
 	import { Description } from './ui/card';
 
 	export let item: any = {};
-	let hovering: boolean = false;
 	let showFullImage: boolean = false;
 
 	async function copyImage(e: MouseEvent) {
@@ -61,34 +60,29 @@
 </script>
 
 <div
-	class="relative h-60 w-60 cursor-pointer overflow-hidden rounded-md bg-slate-100"
-	on:mouseenter={() => (hovering = true)}
-	on:mouseleave={() => (hovering = false)}
+	class="group relative h-60 w-60 cursor-pointer overflow-hidden rounded-md bg-slate-100"
 	on:click={() => (showFullImage = true)}
 >
-	{#if hovering}
-		<!-- overlay -->
-		<div
-			transition:fade={{ duration: 100 }}
-			class="absolute inset-0 flex flex-col items-center justify-center gap-4 bg-black bg-opacity-60 p-4"
-		>
-			<Icon icon="material-symbols:fullscreen-rounded" class="absolute right-2 top-2 h-6 w-6" />
-			<p class="select-none text-white opacity-80">{item[SpongebobData.columns.text]}</p>
-			<div class="gap2 flex items-center">
-				<Button size="icon" variant="ghost" class="text-white" on:click={copyImage}>
-					<Icon class="h-8 w-8" icon="material-symbols:content-copy-outline" />
-				</Button>
-				<Button size="icon" variant="ghost" class="text-white" on:click={downloadImage}>
-					<Icon class="h-8 w-8" icon="material-symbols:download" />
-				</Button>
-			</div>
-		</div>
-	{/if}
 	<img
 		class="h-full object-cover"
 		src={item[SpongebobData.columns.img]}
 		alt={item[SpongebobData.columns.text]}
 	/>
+	<!-- overlay -->
+	<div
+		class="absolute inset-0 flex flex-col items-center justify-center gap-4 bg-black/50 p-4 opacity-0 transition-opacity group-hover:opacity-100"
+	>
+		<Icon icon="material-symbols:fullscreen-rounded" class="absolute right-2 top-2 h-6 w-6" />
+		<p class="select-none text-white opacity-80">{item[SpongebobData.columns.text]}</p>
+		<div class="gap2 flex items-center">
+			<Button size="icon" variant="ghost" class="text-white" on:click={copyImage}>
+				<Icon class="h-8 w-8" icon="material-symbols:content-copy-outline" />
+			</Button>
+			<Button size="icon" variant="ghost" class="text-white" on:click={downloadImage}>
+				<Icon class="h-8 w-8" icon="material-symbols:download" />
+			</Button>
+		</div>
+	</div>
 </div>
 
 <Dialog.Root bind:open={showFullImage}>
